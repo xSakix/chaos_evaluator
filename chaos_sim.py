58f83c15e233cc3f11aaaf274275796485febe7c
xSakix <seckarovci@gmail.com>
2018-02-06 07:43:59 +0100

    - chaos simulation for evaluation of etf results - distribution should be skewed towards a relative mean to found mean...but it's acceptable if it is skewed towards a positive number. If I rank etfs and use the top ones, i'm interested if simulations of those will net positive mean returns over the years. - xetra etf list

---
+++ chaos_sim.py
@@ -0,0 +1,69 @@
+import numpy as np
+import sys
+
+sys.path.insert(0, '../buy_hold_simulation')
+
+from bah_simulator import Investor
+
+
+def yorke(x, r):
+    return x * r * (1. - x)
+
+
+class ChaosSim:
+    def __init__(self, ticket, dist=[1.], tr_cost=2.):
+        self.investor = Investor(ticket,dist)
+        self.tr_cost = tr_cost
+        self.r = np.random.uniform(2.9, 3.9,2)
+        print(self.r)
+
+    def invest(self, data):
+
+        if len(data.keys()) == 0:
+            return
+
+        x = np.array([0.01, 0.01])
+        for _ in range(20):
+            x = yorke(x, self.r)
+
+        self.investor.shares = np.zeros(len(data.keys()))
+
+        day = 0
+
+        for i in data.index:
+            prices = data.loc[i].values
+            if prices == 0.:
+                continue
+            portfolio = self.investor.cash + np.dot(prices, self.investor.shares)
+            if np.isnan(portfolio):
+                portfolio = 0.
+
+            self.investor.history.append(portfolio)
+            self.investor.invested_history.append(self.investor.invested)
+
+            if self.investor.invested == 0:
+                ror = 0
+            else:
+                ror = (portfolio - self.investor.invested) / self.investor.invested
+            self.investor.ror_history.append(ror)
+
+            x = yorke(x, self.r)
+
+            if day % self.investor.dca.period == 0:
+                self.investor.cash += self.investor.dca.cash
+                self.investor.invested += self.investor.dca.cash
+
+            if x[0] >= 0.9 and sum(self.investor.shares > 0) > 0:
+                self.investor.cash = np.dot(self.investor.shares, prices) - sum(self.investor.shares > 0) * self.tr_cost
+                self.investor.shares = np.zeros(len(data.keys()))
+
+            if x[1] >= 0.9 and self.investor.cash > prices:
+                portfolio = self.investor.cash + np.dot(prices, self.investor.shares)
+                c = np.multiply(self.investor.dist, portfolio)
+                c = np.subtract(c, self.tr_cost)
+                s = np.divide(c, prices)
+                s = np.floor(s)
+                self.investor.shares = s
+                self.investor.cash = portfolio - np.dot(self.investor.shares, prices) - len(s) * self.tr_cost
+
+            day += 1
---
+++ test_chaos.py
@@ -0,0 +1,73 @@
+import chaos_sim as chs
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import timeit
+import seaborn as sns
+
+sys.path.insert(0, '../etf_data')
+from etf_data_loader import load_all_data_from_file
+
+
+
+def rolling_mean(data, period):
+    rm = pd.rolling_mean(data, period)
+    rm = rm[~np.isnan(rm)]
+    return rm
+
+
+def mean(value):
+    value = np.mean(value)
+    if np.isnan(value):
+        return 0.
+    return value
+
+
+def compute_one_etf(etf):
+    start_date = '1993-01-01'
+    end_date = '2017-12-31'
+
+    df_adj_close = load_all_data_from_file('etf_data_adj_close.csv', start_date, end_date)
+    sim = chs.ChaosSim(etf)
+    sim.invest(df_adj_close[etf])
+    sim.investor.compute_means()
+
+
+    # _, ax = plt.subplots(3, 1)
+    # for rms in sim.investor.rms_list:
+    #     ax[2].plot(rms)
+    #
+    # print('invested:' + str(sim.investor.invested_history[-1]))
+    # print('value gained:' + str(sim.investor.history[-1]))
+    # print('ror:' + str(sim.investor.ror_history[-1]))
+    # print('mean:' + str(sim.investor.m))
+    # print('std:' + str(sim.investor.std))
+    # for rms in sim.investor.means:
+    #     print(str(rms))
+    #
+    # ax[0].plot(np.log(df_adj_close[etf]))
+    # ax[0].plot(np.log(sim.investor.invested_history))
+    # ax[0].plot(np.log(sim.investor.history))
+    # ax[1].plot(sim.investor.ror_history)
+    # ax[0].legend(['nav', 'invested', 'value'])
+    # ax[1].legend(['RoR'])
+    #
+    # plt.show()
+    return sim.investor
+investors = []
+while len(investors) < 20:
+    investor = compute_one_etf(['SPY'])
+    if investor.cash == investor.invested:
+        continue
+    investors.append(investor)
+    print(len(investors))
+
+means = []
+for investor in investors:
+    means.append(investor.m)
+
+#plt.plot(means)
+sns.kdeplot(means)
+plt.show()
