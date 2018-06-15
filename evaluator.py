829b84e29b3970684f4e2a39f469147245bc3560
xSakix <seckarovci@gmail.com>
2018-06-15 17:50:28 +0200

    evaluation of chaos simulations

--- evaluator.py
+++ evaluator.py
@@ -24,10 +24,11 @@ import random
 
 # -----
 
-prefix = ''
+prefix = 'mil_'
 
 start_date = '1993-01-01'
 end_date = '2017-12-31'
+# end_date = '2017-12-31'
 
 df_adj_close = load_all_data_from_file(prefix + 'etf_data_adj_close.csv', start_date, end_date)
 period = 365
